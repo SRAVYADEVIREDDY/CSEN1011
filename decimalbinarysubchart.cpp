2ad02fb46f86c38ea2a2097eb9765cb83583ac5c
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-11-18 14:43:13 +0530

    Add files via upload

---
+++ decimalbinarysubchart.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   ?? n;
+   ?? index;
+
+   index =1;
+   while (!(n==0))
+   {
+      index =nrem2;
+      index =index+1;
+      n =floor(n/2);
+   }
+
+   return 0;
+}

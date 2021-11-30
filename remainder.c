b6b146c4aec8ecedca0912ff10d87c33781302e4
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-11-30 09:13:54 +0000

    added

---
+++ remainder.c
@@ -0,0 +1,20 @@
+algorithm to find remainder
+1.start
+2.accept dividend
+3.accept divisor
+4.remainder=dividend%divisor
+5.stop
+
+#include<stdio.h>
+int main()
+  {
+    int dividend,divisor, remainder;
+    printf("accept dividend and divisor:");
+    scanf("%d%d dividend ,divisor");
+    remainder=dividend%divisor;
+    printf ("your answer is %d",remainder);
+    return 0; 
+  }
+    
+    
+

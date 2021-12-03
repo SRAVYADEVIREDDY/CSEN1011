688777ec175de1e74fadf69f567a2ff783251279
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-02 22:38:54 -0800

    Update and rename algorithm to caluculate simple and compound intrest.txt to simple intrest.c

---
+++ simple intrest.c
@@ -0,0 +1,30 @@
+Algorithm to caluculate simple and compound intrest
+1.start
+2. enter the principle as p
+3.enter the rate of intrest as r
+4.enter the time period as t
+5.simple intrest=p*t*r/100
+6.compound intrest=p*(1+r/100)*t
+7.end
+
+
+
+#include<stdio.h>
+int main(){
+  int p,t,r;
+
+  printf("principle=");
+  scanf("%d" ,&p);
+
+  printf("time period=");
+scanf("%d" ,&t);
+
+printf("rate of intrest");
+scanf("%d" ,&r);
+
+printf("simple intrest= %d/n",(p*t*r/100) );
+printf("compound intrest=%d/n",(p*(1+r/100)*t ) );
+
+return 0;
+
+}

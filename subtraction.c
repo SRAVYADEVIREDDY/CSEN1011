67c545cf8083340c290d5a29d53a7c69aee70a0a
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-05 15:14:12 +0530

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -7,3 +7,29 @@ c=a-b;
 printf("your answer is %d",c);
 return 0;
 }
+
+excution
+
+include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="Enter a number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   if (a/2==0)
+   {
+      cout << "The number is odd" << endl;   }
+   else
+   {
+      cout << "The number is even" << endl;   }
+
+   return 0;
+}
+
+

6a1d66eb6ec4c5df55375d8bd273138ca7081198
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-05 15:01:01 +0530

    Update and rename algorithm to add to addtion.c

---
+++ addtion.c
@@ -0,0 +1,32 @@
+ALGORITHM TO ADD NUMBERS
+1.START
+2.ACCEPT TWO NUMBERS
+3.PERFORM ADDITION OF TWO NUMBERS
+4.DISPLAY THE RESULT
+5.STOP
+
+addition ccp
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? result;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="Enter first number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="Enter second number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   Result =a+b;
+   cout << "The result of the addition operation is Result" << endl;
+   return 0;
+}
+
+

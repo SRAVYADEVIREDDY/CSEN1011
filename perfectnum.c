e1374b4e7df20074fda41b78ead987484dedd11b
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-07 13:10:47 +0000

    added

---
+++ perfectnum.c
@@ -0,0 +1,18 @@
+#incude<stdio.h>
+int main()
+{
+  int n,i,r,s=0;
+  printf("Enter number");
+  scanf("%d",&n);
+  for(i=1;i<num;i++)
+  {
+    r=n%i;
+    if(r==0)
+    {
+      s=s+i;
+    }
+    if(s=n)
+  }
+
+return 0;
+}

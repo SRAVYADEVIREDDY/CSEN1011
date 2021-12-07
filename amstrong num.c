2d9f2ac842628ce375e3e34d84454565a1324224
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-07 14:39:38 +0530

    Add files via upload

---
+++ amstrong num.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+	{
+	int n,n1,s=0,r;
+	printf("Enter three digit no");
+	scanf("%d",&n);
+	n=n1;
+		while("n1!=0"){
+			r+n1%10;
+	s=s+(r*r*r);
+n1/=10;	
+}
+if("n==s");
+}
+
+
+
+
+	
+	  
+	
+
+
+

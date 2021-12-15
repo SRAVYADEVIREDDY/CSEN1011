9cdaade1aa49b23e432de8adfa55c39145227a50
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-15 06:32:18 +0000

    added

--- numberpyramid.c
+++ numberpyramid.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-  int i,j,rows,alphabet;
+  int i,j,rows,number;
   printf("Enter number of rows:");
   scanf("%d",& rows);
   for(i=1;i<=rows;++i)
   {
     for(j=1;j<=i;++j)
     {
-      printf("%d", alphabet);
+      printf("%d", number);
 
     }
-  ++alphabet;
+  ++number;
   printf("\n");
 }
 return 0;

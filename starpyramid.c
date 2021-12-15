4d2dda7ec390c546013485fe39c3df37dde9f4b6
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-15 06:05:49 +0000

    added

--- starpyramid.c
+++ starpyramid.c
@@ -1,15 +1,15 @@
-#include<studio.h>
+#include<stdio.h>
 int main(){
   int i,j,rows;
 printf("Enter the number of rows:");
 scanf("%d",& rows);
-for(i=1,1<=rows,++i)
+for(i=1;1<=rows;++i)
 {
-  for (j=1,j<=i,++j)
+  for (j=1;j<=i;++j)
   {
     printf("*");
   }
   printf("\n");
 }
-return0;
+return 0;
 }

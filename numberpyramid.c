43002a3adb7801503350612c162dadf51d18a5e6
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-17 06:04:45 +0000

    added

--- numberpyramid.c
+++ numberpyramid.c
@@ -4,10 +4,10 @@ int main()
   int i,j,rows,number;
   printf("Enter number of rows:");
   scanf("%d",& rows);
-  for(i=1;i<=rows;++i)
-  {
-    for(j=1;j<=i;++j)
-    {
+  for(i=1;i<=rows;++i){
+  
+    for(j=1;j<=i;++j){
+    
       printf("%d", number);
 
     }

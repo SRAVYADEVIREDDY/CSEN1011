7d29ef28310abfce40cc446da92f8844817f091e
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-21 09:05:41 +0000

    added

---
+++ flayd'striangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,rows, x;
+  printf("Enter number of rows:")
+  scanf("%d\n"& rows);
+
+    for(i=1;i<=rows;i++)
+  {
+    for(j=1;j<=i,j++)
+    {
+      printf("%d"&x);
+      x++
+    }
+printf("/n");
+  }
+return 0;
+}
--- inversestarpyramid.c
+++ inversestarpyramid.c
@@ -3,9 +3,9 @@ int main(){
   int i,j,rows;
   printf("Enter number of rows:");
   scanf("%d",&rows);
-  for(i=1;i<=rows;i++)
+  for(i=1;i<=rows;i--)
   {
-    for(j=1;i=j;j++)
+    for(j=1;i=j;j--)
    {
      printf("*");
    } 

2d30a1305ead08985c437b47930b935b6d4ee979
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-21 14:21:21 +0000

    added

--- flayd'striangle.c
+++ flayd'striangle.c
@@ -3,16 +3,16 @@ int main()
 {
   int i,j,rows,x=1;
   printf("Enter number of rows:");
-  scanf("%d\n",& rows);
+  scanf("%d",& rows);
 
     for(i=1;i<=rows;i++)
   {
-    for(j=1;j<=i,j++)
+    for(j=1;j<=i;j++)
     {
-      printf("%d",& x);
+      printf("2%d",& x);
       x++;
     }
-printf("/n");
+printf("\n");
   }
 return 0;
 }

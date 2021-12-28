03593fa053cc33a80fbedd35a73d1ffea7946112
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-28 13:57:56 +0000

    added

---
+++ swapadress.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swap(int*x,int*y)
+{
+  int temp
+  temp=*x;
+  *x=*y;
+  *y=*temp;
+}
+int main()
+{
+  int a,b;
+  printf("\n Enter the value of a:");
+  scanf("%d",&a);
+  printf("\n Enter the value of b:");
+  scanf("%d",&b);
+  printf("\n Before swapping a:%d",a);
+  printf("\n Before swapping b:%d",b);
+  swap(&a,b);
+  printf("\n After swapping a:%d",a);
+  printf("\n After swapping b:%d",b);
+  return 0;
+}

0c1da03757283d02c4a050aa9d9d1d886e14b56c
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2021-12-28 09:00:57 +0000

    added

--- swapvalue.c
+++ swapvalue.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
-  int main(){
-    int n1,n2;
-  printf("enter the first number");
-  scanf("%d",&n1);
-  printf("enter the second number");
-  scanf("%d",&n2);
-printf("Before swaping\n1=%d\,n2=%d");
-swap n1,n2;
-printf("after swapping\n1=%d,\n2=%d");
+  int swap (int a, int b)
+  {
+  int d;
+  d=a;
+  a=b;
+  d=b;
+}
+int main()
+{
+int x,y;
+printf("before swapping the number\n",a,b);
+scanf("%d%d",&x,&y)
+swap (x,y);
+printf("after swapping the number\n,x=%d,y=%d",x,y);
+return 0; 
+}
 
- return 0; 
-}
-void swap(int a,int b); 
-int temp
-temp=b;
-a=b;
-printf("values of a and b is%d,%d\n", a,b);
-}

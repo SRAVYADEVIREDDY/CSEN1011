4cb99b262fecd124f173801f344bd7c2f67860ed
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2022-01-04 14:05:06 +0530

    Update 1stquestionsa1.c

--- 1stquestionsa1.c
+++ 1stquestionsa1.c
@@ -1,42 +1,37 @@
 /*Develop a C program to covert upper-case character to a lower-case character and vice versa.
-It should display a message "Incorrect data" if a non-character is given as input.
-Example - If Input is 'a' then Output should be 'A'.
-If Input is 'Z' then Output should be 'z'.
-If input is '1' then Output should be "Incorrect data". */
 
 /* Algorithm
 01 Start
-02. Accept a character 'ch' as input.
-03. If 'ch' is not an alphabet Display "Incorrect Data". 
-Else if 'ch' is lower case character then subtract 32 from the ASCII value of 'ch' Display 'ch'.
-Else add 32 to ASCII value of 'ch' and Display 'ch'
+02. Accept a character 'a' as input.
+03. If 'a' is not an alphabet Display "Incorrect Data". 
+Else if 'a' is lower case character then subtract 32 from the ASCII value of 'a' Display 'a'.
+Else add 32 to ASCII value of 'a' and Display 'a'
 04. Stop  */
 
 # include <stdio.h>
 
 int main(void){
-  char ch;
+  char a;
   printf("Enter a character: ");
-  ch = getchar();
-  if ( (ch <= 122) && (ch >= 97))
+  a = getchar();
+  if ( (a<= 122) && (a>= 97))
     ch -= 32;
-  else if ( (ch <= 90) && (ch >= 65))
+  else if ( (a<= 90) && (a >= 65))
     ch += 32;
   else {
     printf("Incorrect Data\n");
     return 0;
   }
-  printf("Changed case is %c\n", ch);
+  printf("Changed case is %c\n", a);
   return 0;
 }
 /* Output:
-Enter a character: 9
+Enter a character: 543
 Incorrect Data
-Enter a character: a
-Changed case is A
-Enter a character: O
-Changed case is o  */
+Enter a character: d
+Changed case is D
+Enter a character: V
+Changed case is v  */
 
-/* Learnings:
-01. ASCII values (American Standard Code for Information Interchange)
-02. getchar() */
+/* Learning
+else if statements/*

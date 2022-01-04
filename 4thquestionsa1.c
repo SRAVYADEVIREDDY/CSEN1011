4b7ae3d13f892f543f77bcf1664815838f4dc454
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2022-01-04 14:21:18 +0530

    Update 4thquestionsa1.c

--- 4thquestionsa1.c
+++ 4thquestionsa1.c
@@ -11,33 +11,35 @@ Note - Fibonacci Series is 0, 1, 1, 2, 3, 5, 8, 13, ... */
 05. c = a + b, a = b, b = C
 repeat the steps if it is n-2
 06. print c 
-07. Stop.  /*
+07. Stop.
 
-#include<stdio.h>
-int main()
-{
-  int n1=0,n2=1,n3=3,i,n;
-  printf("Enter the numberof elements");
-  scanf("%d",&n);
-  printf("%d%dn",&n1,&n2);
-  for(i=2,i<n;i++);
-  {
-    n3=n2+n1;
-    printf("%d,"n3);
-    n1=n2;
-    n2=n3;
+# include <stdio.h>
+int main(void) {
+  int n, a=0, b=1, c;
+  printf("Enter a positive integer : ");
+  scanf("%d", &n);
 
+  if (n == 1)
+    printf("%d\n", a);
+  else if (n == 2)
+    printf("%d\n", b);
+  else {
+    for (int i = 1; i<= (n-2); i++){
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    printf("%d\n", c);
   }
   return 0;
 }
-
-
-
 
 /* Output:
 Enter a positive integer : 1
 0
 Enter a positive integer : 2
+1
+r a positive integer : 2
 1
 /*Learning outcomes
 if and else if statements/*

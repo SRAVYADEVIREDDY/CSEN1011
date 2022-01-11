7e0a90af7da44a8c3aebbb93851f7a208d53817c
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2022-01-11 08:11:47 +0000

    added

---
+++ array.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+  int marks[5]={7,9,4,13,0};
+  printf("the first value in marks arrayis %d\n ",marks[0]);
+  printf("the second value in marks array is %d\n", marks[1]);
+  printf("the third value in marks array is %d\n", marks[2]);
+  printf("the fourth value in marks array is %d\n",marks[3]);
+  printf("the fifth value in marks array is %d\n",marks[4]);
+  return 0;
+}

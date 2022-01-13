058c104528c62a8d876b66d30bdc2c1bebe226ef
Devireddy Sravya lahari <93367683+SRAVYADEVIREDDY@users.noreply.github.com>
2022-01-13 09:12:05 +0000

    modified

--- module4/array2ex.c
+++ module4/array2ex.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void){
   int marks[5];
-  printf("Enter 5 values for marksarray....\n");
+  printf("Enter 5 values for marks array....\n");
   for(int index=0;index<5;index++)
   scanf("%d",&marks[index]);
   printf("The elements entered are....\n");
@@ -10,3 +10,10 @@ for(int index=0;index<5; index++)
   return 0;
   
 }
+/*output
+Enter 5 values for marks array....
+9 3 6 77 1
+The elements entered are...
+9 3 6 77 1/*
+/* learning outcomes
+declaration of array/*
